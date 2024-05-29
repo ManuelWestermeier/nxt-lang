2b33d8daae3e013c2210498789182ba3fdc4273f
ManuelWestermeier <westermeier111@gmail.com>
2024-05-29 21:38:56 +0200

    update

--- publish.bat
+++ publish.bat
@@ -1,6 +1,5 @@
-git init
 git add .
-git commit -m "first commit"
+git commit -m "update"
 git branch -M main
 git remote add origin https://github.com/ManuelWestermeier/nxt-lang.git
 git push -u origin main
